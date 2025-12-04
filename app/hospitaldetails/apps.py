c18a6ad3fa312490eed523cc5b99aae693498d8d
jamesbiggs <james.biggs@nationalarchives.gov.uk>
2025-12-04 18:23:58 +0000

    Update apps.py file

--- app/hospitaldetails/apps.py
+++ app/hospitaldetails/apps.py
@@ -3,4 +3,5 @@ from django.apps import AppConfig
 
 class HospitaldetailsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'hospitaldetails'
+    name = 'app.hospitaldetails'
+    verbose_name = "Hospital Details"
