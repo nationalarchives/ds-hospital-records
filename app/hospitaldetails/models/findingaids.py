734b9a7ff91e94f4065228d81c8e915ef7e85ab1
jamesbiggs <james.biggs@nationalarchives.gov.uk>
2025-12-04 18:26:56 +0000

    Create FindingAids and FindingAidsLocation models

---
+++ app/hospitaldetails/models/findingaids.py
@@ -0,0 +1,21 @@
+from django.db import models
+
+class FindingAids(models.Model):
+    value = models.CharField(max_length=25)
+
+    def __str__(self):
+        return self.value
+    
+    class Meta:
+        verbose_name = "Finding Aid"
+        verbose_name_plural = "Finding Aids"
+
+class FindingAidsLocation(models.Model):
+    value = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.value
+    
+    class Meta:
+        verbose_name = "Finding Aids Location"
+        verbose_name_plural = "Finding Aids Locations"
