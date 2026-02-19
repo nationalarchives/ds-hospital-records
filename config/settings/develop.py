d5a275caa7e37053021e08acc5e8bbb1bdb0312c
jamesbiggs <james.biggs@nationalarchives.gov.uk>
2026-02-19 13:41:24 +0000

    Refactor debug toolbar activation to bypass during tests

--- config/settings/develop.py
+++ config/settings/develop.py
@@ -1,4 +1,5 @@
 import os
+import sys
 
 from config.util import strtobool
 
@@ -11,7 +12,8 @@ DEBUG: bool = strtobool(os.getenv("DEBUG", "False"))
 
 SENTRY_SAMPLE_RATE = float(os.getenv("SENTRY_SAMPLE_RATE", "1.0"))
 
-if DEBUG:
+IS_TESTING = any(arg in {"test", "pytest"} for arg in sys.argv) # Bypass debug toolbar when running tests, DJDT can't be used with tests (debug_toolbar.E001)
+if DEBUG and not IS_TESTING:
     try:
         import debug_toolbar
 
