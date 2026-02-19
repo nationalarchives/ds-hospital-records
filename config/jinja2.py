77c5f0075359a25324898d47fc94d32ea141966c
jamesbiggs <james.biggs@nationalarchives.gov.uk>
2026-02-19 13:43:00 +0000

    Create abbr templatetag to apply <abbr> tags to specified regex patterns to give contextual hints to users for abbreviations (e.g. c.1940 is circa 1940)

--- config/jinja2.py
+++ config/jinja2.py
@@ -7,6 +7,7 @@ from django.conf import settings
 from django.templatetags.static import StaticNode
 from django.urls import reverse
 from django.utils.safestring import mark_safe
+from markupsafe import Markup
 from jinja2 import Environment
 
 register = template.Library()
@@ -55,6 +56,80 @@ def linebreaksbr(text):
     return mark_safe(text.replace("\n", "<br>"))
 
 
+ABBR_PATTERNS = [
+    {"pattern": r"\bc\.(\s*\d{1,4})", "title": "circa", "abbr": "c.", "suffix_group": 1},
+    {
+        "pattern": r"\b(PLI)(\s*:)",
+        "title": "Poor Law Institution",
+        "abbr_group": 1,
+        "suffix_group": 2,
+    },
+    {
+        "pattern": r"\b(LA)(\s*:)",
+        "title": "Local Authority",
+        "abbr_group": 1,
+        "suffix_group": 2,
+    },
+    {"pattern": r"\b(AC)(\s*:)", "title": "Acute", "abbr_group": 1, "suffix_group": 2},
+    {
+        "pattern": r"\b(GER)(\s*:)",
+        "title": "Geriatric",
+        "abbr_group": 1,
+        "suffix_group": 2,
+    },
+    {
+        "pattern": r"\b(LRO)(\s*:)",
+        "title": "Local Record Office",
+        "abbr_group": 1,
+        "suffix_group": 2,
+    },
+    {"pattern": r"\b(AR)(\s*:)", "title": "Repository", "abbr_group": 1, "suffix_group": 2},
+    {
+        "pattern": r"\b(NRA)(\s*:)",
+        "title": "National Register of Archives",
+        "abbr_group": 1,
+        "suffix_group": 2,
+    },
+    {"pattern": r"\b(C)(\s*:)", "title": "Children", "abbr_group": 1, "suffix_group": 2},
+    {
+        "pattern": r"\b(CAT)(\s*:)",
+        "title": "Catalogue",
+        "abbr_group": 1,
+        "suffix_group": 2,
+    },
+    {"pattern": r"\b(VOL)(\s*:)", "title": "Voluntary", "abbr_group": 1, "suffix_group": 2},
+    {"pattern": r"\b(MNT)(\s*:)", "title": "Mental", "abbr_group": 1, "suffix_group": 2},
+]
+
+
+def abbr(value):
+    if not value:
+        return value
+
+    text = str(value)
+    changed = False
+    for rule in ABBR_PATTERNS:
+        def replacer(match):
+            if "abbr" in rule:
+                abbreviation = rule["abbr"]
+            else:
+                abbreviation = match.group(rule["abbr_group"])
+            suffix = ""
+            if "suffix_group" in rule:
+                suffix = match.group(rule["suffix_group"])
+            return f'<abbr title="{rule["title"]}">{abbreviation}</abbr>{suffix}'
+
+        text, replacements = re.subn(rule["pattern"], replacer, text)
+        if replacements:
+            changed = True
+
+    if changed:
+        return mark_safe(text)
+    if isinstance(value, Markup):
+        return value
+    return text
+
+
 def environment(**options):
     env = Environment(**options)
 
@@ -86,5 +161,5 @@ def environment(**options):
             "now_iso_8601": now_iso_8601,
         }
     )
-    env.filters.update({"slugify": slugify, "linebreaksbr": linebreaksbr})
+    env.filters.update({"slugify": slugify, "linebreaksbr": linebreaksbr, "abbr": abbr})
     return env
