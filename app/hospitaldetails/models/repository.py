d0da8f2e6a5edc4368adb7331c834d1b6a1327c4
James Biggs <62654785+jamesbiggs@users.noreply.github.com>
2026-02-26 18:21:51 +0000

    Repository ARCHON updates (#9)
    * Add archon_url property to Repository
    
    * Add full_address property to Repository model
    
    * Remove repository_list view and redirect to archon_url in repository_detail
    
    * Refactor hospital details templates: link to ARCHON page if exists, otherwise internal repository page (maybe remove?)
    
    * Formatting
    
    * Update test

--- app/hospitaldetails/models/repository.py
+++ app/hospitaldetails/models/repository.py
@@ -17,5 +17,28 @@ class Repository(NH3CleanSaveMixin, models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     last_updated_at = models.DateTimeField(auto_now=True)
 
+    @property
+    def full_address(self):
+        """
+        Returns the full address of the repository as a single string, combining all available address fields.
+        """
+        address_parts = [
+            self.street_1,
+            self.street_2,
+            self.town,
+            self.county,
+            self.postcode,
+        ]
+        return ", ".join(part for part in address_parts if part)
+
+    @property
+    def archon_url(self):
+        """
+        Returns the URL to the repository's page on Discovery if an ARCHON code is present.
+        """
+        if self.archon_code:
+            return f"https://discovery.nationalarchives.gov.uk/details/a?_ref={self.archon_code}"
+        return None
+
     def __str__(self):
         return self.name
--- app/hospitaldetails/urls.py
+++ app/hospitaldetails/urls.py
@@ -8,6 +8,5 @@ urlpatterns = [
     path("", views.home_page, name="home_page"),
     path("hospitals", views.search, name="search"),
     path("hospitals/<int:id>", views.hospital_detail, name="hospital_detail"),
-    path("repositories", views.repository_list, name="repository_list"),
     path("repositories/<int:id>", views.repository_detail, name="repository_detail"),
 ]
