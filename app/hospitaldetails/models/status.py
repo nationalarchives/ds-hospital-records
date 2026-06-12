fbce2d11b94d9c7879665ab31b2303e778597095
jamesbiggs <james.biggs@nationalarchives.gov.uk>
2026-06-12 12:03:21 +0100

    Migrations

--- app/hospitaldetails/models/status.py
+++ app/hospitaldetails/models/status.py
@@ -18,10 +18,10 @@ class Pre1948Status(NH3CleanSaveMixin, models.Model):
 class Post1948Status(NH3CleanSaveMixin, models.Model):
     value = models.CharField(max_length=50)
 
+    def __str__(self):
+        return self.value
+
     class Meta:
         ordering = ["value"]
         verbose_name = "Post-1948 Status"
         verbose_name_plural = "Post-1948 Statuses"
-
-    def __str__(self):
-        return self.value
