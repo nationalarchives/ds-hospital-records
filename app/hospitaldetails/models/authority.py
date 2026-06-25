002ac982ab654b90b23489cce09d9db49102535d
James Biggs <62654785+jamesbiggs@users.noreply.github.com>
2026-06-25 14:08:48 +0100

    Add verbose names and ordering to models (#21)
    * Add verbose names and ordering to models
    
    * Migrations
    
    * Bump PR

--- app/hospitaldetails/models/authority.py
+++ app/hospitaldetails/models/authority.py
@@ -9,12 +9,22 @@ class RegionalBoard(NH3CleanSaveMixin, models.Model):
     def __str__(self):
         return self.name
 
+    class Meta:
+        ordering = ["name"]
+        verbose_name = "Regional Board"
+        verbose_name_plural = "Regional Boards"
+
 
 class ManagementCommittee(NH3CleanSaveMixin, models.Model):
     name = models.CharField(max_length=100)
 
     def __str__(self):
         return self.name
+
+    class Meta:
+        ordering = ["name"]
+        verbose_name = "Management Committee"
+        verbose_name_plural = "Management Committees"
 
 
 class Pre1982RegionalAuthority(NH3CleanSaveMixin, models.Model):
@@ -23,12 +33,22 @@ class Pre1982RegionalAuthority(NH3CleanSaveMixin, models.Model):
     def __str__(self):
         return self.name
 
+    class Meta:
+        ordering = ["name"]
+        verbose_name = "Pre-1982 Regional Authority"
+        verbose_name_plural = "Pre-1982 Regional Authorities"
+
 
 class Post1982RegionalAuthority(NH3CleanSaveMixin, models.Model):
     name = models.CharField(max_length=100)
 
     def __str__(self):
         return self.name
+
+    class Meta:
+        ordering = ["name"]
+        verbose_name = "Post-1982 Regional Authority"
+        verbose_name_plural = "Post-1982 Regional Authorities"
 
 
 class Pre1982DistrictAuthority(NH3CleanSaveMixin, models.Model):
@@ -37,9 +57,19 @@ class Pre1982DistrictAuthority(NH3CleanSaveMixin, models.Model):
     def __str__(self):
         return self.name
 
+    class Meta:
+        ordering = ["name"]
+        verbose_name = "Pre-1982 District Authority"
+        verbose_name_plural = "Pre-1982 District Authorities"
+
 
 class Post1982DistrictAuthority(NH3CleanSaveMixin, models.Model):
     name = models.CharField(max_length=100)
 
     def __str__(self):
         return self.name
+
+    class Meta:
+        ordering = ["name"]
+        verbose_name = "Post-1982 District Authority"
+        verbose_name_plural = "Post-1982 District Authorities"
